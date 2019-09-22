d870cfe56722392bad84470b1278e3c7b14e98fa
jakesIII <jayruma@yahoo.com>
2019-09-23 02:54:24 +0300

    Add HTML structure

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function myFunction(){
+    var date = parseFloat(prompt("Your Date of Birth:"));
+    var month = parseFloat(prompt("Your Month of Birth:"));
+    var year = parseFloat(prompt("Your Year of Birth:"));
+}
+
+var gender = function() {
+    document.getElementId('gender');
+};
+
+var maleName = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var femaleName = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
